f35e51748fdd88c0046d440fd784d0cfa8bab36a
Thao.Hoang <thaohm@c3anywhere.(none)>
2017-12-19 21:00:24 +0700

    [f] Blinking text app

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
-class Greeting extends Component {
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showText: true };
+
+    setInterval(() => {
+      this.setState(previousState => {
+        return { showText: !previousState.showText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.showText ? this.props.text : '';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Text>{display}</Text>
     );
   }
 }
 
-export default class LotsOfGreeting extends Component {
+export default class BlinkApp extends Component {
   render() {
     return (
-      <View style={{ alignItems: 'center' }}>
-        <Greeting name="Rexxa"></Greeting>
-        <Greeting name="Jaina"></Greeting>
-        <Greeting name="Valeera"></Greeting>
+      <View>
+        <Blink text="I love to blink"></Blink>
+        <Blink text="Yes blinking is so great"></Blink>
+        <Blink text="Why did they ever take this out of HTML"></Blink>
+        <Blink text="Look at me look at me look at me"></Blink>
       </View>
     )
   }
