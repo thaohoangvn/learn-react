9cb0b1587158b33329619f4296466df944a21a99
Thao.Hoang <thaohm@c3anywhere.(none)>
2017-12-19 21:17:29 +0700

    [f] Flex Direction Basics

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
-export default class FlexDimensionsBasics extends Component {
+export default class FlexDirectionBasics extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
-        <View style={{flex: 1, backgroundColor: 'powderblue'}}></View>
-        <View style={{flex: 2, backgroundColor: 'skyblue'}}></View>
-        <View style={{flex: 3, backgroundColor: 'steelblue'}}></View>
+      <View style={{flex: 1, flexDirection: 'row'}}>
+        <View style={{width: 70, height: 50, backgroundColor: 'powderblue'}}></View>
+        <View style={{width: 70, height: 50, backgroundColor: 'skyblue'}}></View>
+        <View style={{width: 70, height: 50, backgroundColor: 'steelblue'}}></View>
       </View>
     );
   }
