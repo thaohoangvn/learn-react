c38356d5f0089292aedde4878b487e7d1d950b9b
Thao.Hoang <thaohm@c3anywhere.(none)>
2017-12-19 21:20:20 +0700

    [f] Justify Content Basics

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
-export default class FlexDirectionBasics extends Component {
+export default class JustifyContentBasics extends Component {
   render() {
     return (
-      <View style={{flex: 1, flexDirection: 'row'}}>
+      <View style={{flex: 1,
+          flexDirection: 'column',
+          justifyContent: 'space-between'
+      }}>
         <View style={{width: 70, height: 50, backgroundColor: 'powderblue'}}></View>
         <View style={{width: 70, height: 50, backgroundColor: 'skyblue'}}></View>
         <View style={{width: 70, height: 50, backgroundColor: 'steelblue'}}></View>
