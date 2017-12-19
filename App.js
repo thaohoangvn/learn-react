08c2d683b6d4d12cae55682b8670b14e98f4f5b7
Thao.Hoang <thaohm@c3anywhere.(none)>
2017-12-19 21:08:00 +0700

    Flex Dimensions Blocks

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
-export default class FixedDimesionBasics extends Component {
+export default class FlexDimensionsBasics extends Component {
   render() {
     return (
-      <View>
-        <View style={{width: 50, height: 50, backgroundColor: 'powderblue' }}></View>
-        <View style={{width: 100, height: 100, backgroundColor: 'skyblue' }}></View>
-        <View style={{width: 150, height: 150, backgroundColor: 'steelblue' }}></View>
+      <View style={{flex: 1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}></View>
+        <View style={{flex: 2, backgroundColor: 'skyblue'}}></View>
+        <View style={{flex: 3, backgroundColor: 'steelblue'}}></View>
       </View>
     );
   }
