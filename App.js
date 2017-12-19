744d3796cc9d1ff41114dcd647d94705c3857695
Thao.Hoang <thaohm@c3anywhere.(none)>
2017-12-19 21:04:24 +0700

    Fixed Dimension Blocks

--- App.js
+++ App.js
@@ -1,35 +1,14 @@
 import React, { Component } from 'react';
 import { AppRegistry, Image, Text, View } from 'react-native';
 
-class Blink extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { showText: true };
-
-    setInterval(() => {
-      this.setState(previousState => {
-        return { showText: !previousState.showText };
-      });
-    }, 1000);
-  }
-
-  render() {
-    let display = this.state.showText ? this.props.text : '';
-    return (
-      <Text>{display}</Text>
-    );
-  }
-}
-
-export default class BlinkApp extends Component {
+export default class FixedDimesionBasics extends Component {
   render() {
     return (
       <View>
-        <Blink text="I love to blink"></Blink>
-        <Blink text="Yes blinking is so great"></Blink>
-        <Blink text="Why did they ever take this out of HTML"></Blink>
-        <Blink text="Look at me look at me look at me"></Blink>
+        <View style={{width: 50, height: 50, backgroundColor: 'powderblue' }}></View>
+        <View style={{width: 100, height: 100, backgroundColor: 'skyblue' }}></View>
+        <View style={{width: 150, height: 150, backgroundColor: 'steelblue' }}></View>
       </View>
-    )
+    );
   }
 }
